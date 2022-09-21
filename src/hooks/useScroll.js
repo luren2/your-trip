bc14d0431fb2f96091a4ffab541fd9438c9ed382
luren2 <1215750724@qq.com>
2022-09-21 23:50:38 +0800

    滚动显示搜索框

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,34 +1,37 @@
 import { onActivated, onDeactivated, onMounted, onUnmounted, ref } from 'vue';
+import { throttle } from 'underscore';
 
 export default function useScroll() {
   const isReachBottom = ref(false);
+  const scrollTop = ref(0);
 
-  const listenScroll = () => {
-    const scrollTop = document.documentElement.scrollTop;
+  // 节流
+  const scrollListenerHandler = throttle(() => {
+    scrollTop.value = document.documentElement.scrollTop;
     const scrollHeight = document.documentElement.scrollHeight;
     const clientHeight = document.documentElement.clientHeight;
     // 有误差 取整 精度问题
-    if (parseInt(scrollHeight - scrollTop) <= clientHeight) {
-      console.log('滚到底部了!!!');
+    if (parseInt(scrollHeight - scrollTop.value) <= clientHeight) {
+      // console.log('滚到底部了!!!');
       isReachBottom.value = true;
     }
-  };
+  }, 100);
 
   onMounted(() => {
-    window.addEventListener('scroll', listenScroll);
+    window.addEventListener('scroll', scrollListenerHandler);
   });
 
   onUnmounted(() => {
-    window.removeEventListener('scroll', listenScroll);
+    window.removeEventListener('scroll', scrollListenerHandler);
   });
 
   // keep-live
   onActivated(() => {
-    window.addEventListener('scroll', listenScroll);
+    window.addEventListener('scroll', scrollListenerHandler);
   });
   onDeactivated(() => {
-    window.removeEventListener('scroll', listenScroll);
+    window.removeEventListener('scroll', scrollListenerHandler);
   });
 
-  return { isReachBottom };
+  return { isReachBottom, scrollTop };
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,12 +6,13 @@
     </div>
     <HomeSearchBox />
     <HomeCategories />
+    <div class="search-bar" v-if="isShowSearchBar">搜索框</div>
     <HomeContent />
   </div>
 </template>
 
 <script setup>
-import { watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import HomeNavBar from './cpns/HomeNavBar.vue';
 import HomeSearchBox from './cpns/HomeSearchBox.vue';
 import HomeCategories from './cpns/HomeCategories.vue';
@@ -26,7 +27,7 @@ homeStore.fetchCategories();
 homeStore.fetchHouseList();
 
 //监听是否滚动到底部
-const { isReachBottom } = useScroll();
+const { isReachBottom, scrollTop } = useScroll();
 watch(isReachBottom, (newValue) => {
   if (newValue) {
     // 数据加载出来再赋值
@@ -34,6 +35,15 @@ watch(isReachBottom, (newValue) => {
       isReachBottom.value = false;
     });
   }
+});
+
+// 搜索框显示的控制
+// const isShowSearchBar = ref(false);
+// watch(scrollTop, (newValue) => {
+//   isShowSearchBar.value = newValue >= 100;
+// });
+const isShowSearchBar = computed(() => {
+  return scrollTop.value >= 100;
 });
 </script>
 
